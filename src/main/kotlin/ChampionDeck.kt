9fa20b447a85692f8aa9b83a2626ba68dddc734f
donghui <eastshine0221@gmail.com>
2023-10-25 00:37:25 +0900

    Add test for level 1 rule

--- src/main/kotlin/ChampionDeck.kt
+++ src/main/kotlin/ChampionDeck.kt
@@ -1,18 +1,48 @@
 
 typealias ChampionSet = List<Champion>
 class ChampionDeck {
-    fun draw(playerLevel: Long): ChampionSet {
+    private val championPoll: Map<ChampionCost, MutableList<Champion>> = mapOf(
+        ChampionCost.ONE to mutableListOf(),
+        ChampionCost.TWO to mutableListOf(),
+        ChampionCost.THREE to mutableListOf(),
+        ChampionCost.FOUR to mutableListOf(),
+        ChampionCost.FIVE to mutableListOf(),
+    )
+
+    fun reroll(playerLevel: Long, preSet: ChampionSet?): ChampionSet {
+        return listOf(
+            Champion.Cassiopeia,
+            Champion.ChoGath,
+            Champion.Graves,
+            Champion.Illaoi,
+            Champion.Illaoi,
+        )
+    }
+
+    fun returnDeadPlayerChampionSet(deadPlayerChampionSet: ChampionSet) {
+        return Unit
+    }
+
+    fun listAllChampionsByCost(cost: ChampionCost): ChampionSet {
         return listOf()
+    }
+
+    fun countRemainChampionsByCost(cost: ChampionCost): Long {
+        return 0
     }
 }
 
+enum class ChampionCost {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+}
 
-class Champion(val cost: ChampionCost)
-
-enum class ChampionCost(val cost: Long) {
-    ONE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
+enum class Champion(val cost: ChampionCost, name: String) {
+    Cassiopeia(ChampionCost.ONE, "카시오페아"),
+    ChoGath(ChampionCost.ONE, "초가스"),
+    Graves(ChampionCost.ONE, "그레이브즈"),
+    Illaoi(ChampionCost.ONE, "일라오이"),
 }
--- src/test/kotlin/ChampionDeckTest.kt
+++ src/test/kotlin/ChampionDeckTest.kt
@@ -1,16 +1,35 @@
 import org.junit.jupiter.api.Test
 import kotlin.test.DefaultAsserter.assertEquals
+import kotlin.test.assertTrue
 
 class ChampionDeckTest {
     @Test
-    fun `기본 뽑기 기능 테스트`() {
+    fun `한 세트의 사이즈는 5장이어야한다`() {
         val sut = ChampionDeck()
 
-        val result = sut.draw(1)
+        val result = sut.reroll(1, null)
 
         assertEquals("뽑은 세트의 사이즈는 5여야한다.", 5, result.size)
-        result.forEach {
-            assert(it.cost == ChampionCost.ONE)
-        }
+    }
+
+    @Test
+    fun `1렙에는 1코스트 챔피언만 나와야한다`() {
+        val sut = ChampionDeck()
+
+        val result = sut.reroll(1, null)
+
+        assertTrue { result.all { it.cost == ChampionCost.ONE } }
+    }
+
+    @Test
+    fun `리롤을 한 후, 플레이어 상점에 있는 챔피언은 빠져야한다`() {
+        val sut = ChampionDeck()
+        val preCount = sut.listAllChampionsByCost(ChampionCost.ONE).size
+
+        val result = sut.reroll(1, null)
+        assertTrue { result.all { it.cost == ChampionCost.ONE } }
+
+        val afterCount = sut.listAllChampionsByCost(ChampionCost.ONE).size
+        assertTrue { preCount - afterCount == 5 }
     }
 }
